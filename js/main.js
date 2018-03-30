40c4ee444c3acdcbd247d0f467329d8687115dfc
Tomasz Ostrowski <ostrowto@gmail.com>
2018-03-30 21:35:53 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+function dis(x) {
+    frm.txt.value = frm.txt.value + x;
+}
+
+
+function ce() {
+    frm.txt.value = '';
+
+
+}
+
+
+function BS() {
+    frm.txt.value = frm.txt.value.slice(0, frm.txt.value.length - 1);
+}
+
+function pi() {
+    frm.txt.value = Math.PI;
+}
+
